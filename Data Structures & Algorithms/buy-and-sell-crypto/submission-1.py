97ced7d8eb0bed8e39924d4ebb4654d37dc22c44
Francis Garcia <112897002+fgarcia06@users.noreply.github.com>
2026-08-08 16:00:27 -0600

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # brute force solution
+        max_profit = 0
+        min_price = 0
+        for i in range(len(prices)): 
+            if i == 0:
+                min_price = prices[0]
+
+            if prices[i] < min_price:
+                min_price = prices[i]
+
+            else: # current price is greater than the min price (profit possible)
+                profit = prices[i] - min_price
+                if profit > max_profit:
+                    max_profit = profit
+
+        return max_profit
